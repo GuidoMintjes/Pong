ebce5196a5e3235719c2186afa016a7cc9e2b950
Guido Mintjes <guidomintjes@gmail.com>
2021-09-22 14:28:09 +0200

    Geachte Heer van de Vorle
    Bij deze alles wat nog veranderd is, je zit in mijn live share dus het boeit geen ene reet, maar hier heb je het alsnog.
    
    Met de allervriendelijkste groeten en het grootste succes met deze opdracht,
    GuidoCraftGamer

--- Pong/Game1.cs
+++ Pong/Game1.cs
@@ -7,45 +7,81 @@ namespace Pong {
     public class Game1 : Game {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        public Color colourBrown = new Color(200, 150, 100, 255);
+
+        Texture2D ball;
+        Texture2D test;
+
+        MouseState mouse;
+        Vector2 cursorSpeed, cursorPos, cursorLastPos;
+
+
 
         public Game1() {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            IsMouseVisible = false;
         }
+
+
 
         protected override void Initialize() {
 
             //Set window screen sizes
             _graphics.IsFullScreen = true;
-            _graphics.PreferredBackBufferWidth = 1;
-            _graphics.PreferredBackBufferHeight = 1;
+            _graphics.PreferredBackBufferWidth = GraphicsDevice.DisplayMode.Width;
+            _graphics.PreferredBackBufferHeight = GraphicsDevice.DisplayMode.Height;
             _graphics.ApplyChanges();
 
             base.Initialize();
         }
 
+
+
         protected override void LoadContent() {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            ball = Content.Load<Texture2D>("Sprites/bal");
+            test = Content.Load<Texture2D>("Sprites/test");
+
+
         }
 
+
+
         protected override void Update(GameTime gameTime) {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || 
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
                 Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            // Gets the state of the mouse, allowing us to get its location
+            mouse = Mouse.GetState();
 
+            cursorPos = new Vector2(mouse.X, mouse.Y);
+            if (cursorLastPos == new Vector2(0, 0)) { base.Update(gameTime); } else {
+
+                cursorSpeed = new Vector2(Math.Abs(cursorPos.X - cursorLastPos.X),
+                    Math.Abs(cursorPos.Y - cursorLastPos.Y));
+
+                /*Console.WriteLine("Speed of cursor is: \tX-axis:" +
+                    cursorSpeed.X + ", \tY-axis:" + cursorSpeed.Y);*/
+            }
+
+            cursorLastPos = cursorPos;
             base.Update(gameTime);
         }
 
-        protected override void Draw(GameTime gameTime) {
-            GraphicsDevice.Clear(colourBrown);
 
-            // TODO: Add your drawing code here
+
+        protected override void Draw(GameTime gameTime) {
+
+            GraphicsDevice.Clear(Color.Transparent);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(test, new Vector2(0, 0), Color.White);
+            _spriteBatch.Draw(ball, new Vector2(mouse.X, mouse.Y), Color.White);
+            _spriteBatch.End();
+
 
             base.Draw(gameTime);
         }
